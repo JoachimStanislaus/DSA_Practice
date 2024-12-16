b4ac941b0b89117103d4c0553b298aae036368f7
Joachim Tan <joachim@Joachims-Mac-mini.local>
2024-12-16 22:47:32 +0000

    added answers for LL

--- Linked_Lists/Linked_Lists.py
+++ Linked_Lists/Linked_Lists.py
@@ -1,18 +1,90 @@
 # 5. LINKED LISTS
+
 # A Linked List is a linear data structure where each element (node) contains:
 # 1. Data (the value to store)
 # 2. A reference (or pointer) to the next node
+class LLNode:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+
 
 # Types of Linked Lists:
 # - Singly Linked List
-# - Doubly Linked List
-# - Circular Linked List
+class SLLNode:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+
+# - Doubly Linked List (able to traverse backwards or forwards)
+class DLLNode:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+        self.prev = None
+
+# - Circular Linked List (last node will point back to self.head (first node))
+class CLLNode:
+    def __init__(self,data):
+        self.data = data
+        self.next = self.head
+
 
 # 5.1 Singly Linked List
-# - Insert at the beginning
-# - Insert at the end
-# - Delete by value
-# - Print the linked list
+class SingleLinkedList:
+    def __init__(self):
+        self.head = None
+
+    # - Insert at the beginning
+    def insert_at_start(self,data):
+        new_node = SLLNode(data)
+        new_node.next = self.head
+        self.head = new_node
+
+    # - Insert at the end
+    def insert_at_end(self,data):
+        new_node = SLLNode(data)
+        if not self.head:
+            self.head = new_node
+            return
+        temp = self.head
+        while temp.next:
+            temp = temp.next
+        temp.next = new_node
+
+    # - Delete by value
+    def delete_by_value(self,value):
+        if not self.head: # Case 1: List is empty
+            return
+        temp = self.head
+        prev = None
+
+        # Traverse the list to find the node
+        while temp.data != value:
+            prev = temp
+            temp = temp.next
+
+        # Case 2: Value not found
+        if not temp:
+            return
+        
+        # Case 3: Head node needs to be deleted
+        if temp == self.head:
+            self.head = temp.next
+            return
+
+        # Case 4: Delete the node in the middle or end
+        prev.next = temp.next
+
+
+    # - Print the linked list
+    def print_LL(self):
+        temp = self.head
+        while temp.next:
+            print(temp.data)
+            temp = temp.next
+        print('None')
+
 
 # 5.2 Doubly Linked List
 # - Insert at the end
