5c380d4b87bfc49864507f99fa9429ba646de705
Joachim Tan <joachim@Joachims-Mac-mini.local>
2024-12-28 21:01:59 +0000

    added questions

--- Linked_Lists/Linked_Lists.py
+++ Linked_Lists/Linked_Lists.py
@@ -87,11 +87,9 @@ class SingleLinkedList:
 
 
 # 5.2 Doubly Linked List
-
-class DoubleLinkedList:
+class DoubleLinkedList ():
     def __init__(self):
         self.head = None
-
 # - Insert at the end
     def insert_at_end(self,data):
         new_node = DLLNode(data)
@@ -99,15 +97,25 @@ class DoubleLinkedList:
             self.head = new_node
             return
         temp = self.head
-        while temp.next:
+        while temp != None:
             temp = temp.next
         temp.next = new_node
         new_node.prev = temp
-
 # - Print the linked list
-    def print_LL(self):
+    def print_linked_list(self,data):
         temp = self.head
-        while temp.next:
+        if not self.head:
+            print('empty list')
+            return
+        while temp != None:
             print(temp.data)
             temp = temp.next
         print('None')
+
+
+# 6. PRACTICE QUESTIONS
+# 1. Implement a Singly Linked List with insert, delete, and print methods.
+# 2. Reverse a Singly Linked List.
+# 3. Find the Middle Element of a Linked List.
+# 4. Detect a Cycle in a Linked List.
+# 5. Implement a Doubly Linked List with insert and print methods.
