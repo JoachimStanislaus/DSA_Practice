2eaac0732216463898c2891552980e65284536d3
Joachim Tan <joachim@Joachims-Mac-mini.local>
2025-01-05 14:19:10 +0000

    lc qns answers

---
+++ LC_Practice/605_Can_Place_Flowers.py
@@ -0,0 +1,45 @@
+# 605. Can Place Flowers
+
+# You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be planted in adjacent plots.
+
+# Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, and an integer n, return true if n new flowers can be planted in the flowerbed without violating the no-adjacent-flowers rule and false otherwise.
+
+# Example 1:
+
+# Input: flowerbed = [1,0,0,0,1], n = 1
+# Output: true
+# Example 2:
+
+# Input: flowerbed = [1,0,0,0,1], n = 2
+# Output: false
+
+# Constraints:
+
+# 1 <= flowerbed.length <= 2 * 104
+# flowerbed[i] is 0 or 1.
+# There are no two adjacent flowers in flowerbed.
+# 0 <= n <= flowerbed.length
+
+class Solution(object):
+    def canPlaceFlowers(self, flowerbed, n):
+        """
+        :type flowerbed: List[int]
+        :type n: int
+        :rtype: bool
+        """
+        flowerbedclone = flowerbed
+        plantspotCount = 0
+        for i in range(len(flowerbed)):
+            if i > 0 and i < len(flowerbedclone)-1:
+                if flowerbedclone[i-1] == 0 and flowerbedclone[i+1] == 0 and flowerbedclone[i] == 0:
+                    flowerbedclone[i] = 1
+                    plantspotCount += 1
+            elif i == len(flowerbed)-1:
+                if flowerbedclone[i-1] == 0 and flowerbedclone[i] == 0:
+                    flowerbedclone[i] = 1
+                    plantspotCount += 1
+            else:
+                if flowerbedclone[i+1] == 0 and flowerbedclone[i] == 0:
+                    flowerbedclone[i] = 1
+                    plantspotCount += 1
+        return plantspotCount >= n
