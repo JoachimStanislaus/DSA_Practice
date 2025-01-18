bf84deb8d47d0640c2239e48a981f68bf9b92e3e
Joachim Tan <joachim@Joachims-Mac-mini.local>
2025-01-18 10:04:46 +0000

    weird python 2 note

---
+++ Impt_Basics/math.py
@@ -0,0 +1,5 @@
+
+# For python 2 the difference will be rounded to 0 as in python 2 dividing 2 integers performs integer division by default which truncates the result to an int
+
+difference = 2/3
+print(difference)
