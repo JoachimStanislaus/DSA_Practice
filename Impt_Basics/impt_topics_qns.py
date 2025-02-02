6ebbc6e96c81dfdff1a2719a7dfa44bcb69b84ac
Joachim Tan <joachim@Joachims-Mac-mini.local>
2025-02-02 19:24:15 +0000

    some sample practice questions that are impt

---
+++ Impt_Basics/impt_topics_qns.py
@@ -0,0 +1,13 @@
+# Explain what OOP is in your own words
+
+# Explain the 4 concepts of OOP, Inheritance, Encapsulation, Abstraction, Polymorphism
+
+# Explain what Multithreading and Multiprocessing is?
+
+# Explain what Async and Synchronous is
+
+# Explain what class and static class methods are
+
+# Does python pass by reference or value?
+
+# How do you make a copy of a string?
